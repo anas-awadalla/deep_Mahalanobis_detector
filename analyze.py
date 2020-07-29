38e3f4e94a1d237d17aab720ca10fbca1be3d772
Anas Awadalla <anasa2@cs.washington.edu>
2020-07-29 10:20:28 -0700

    added image analysis

--- analyze.py
+++ analyze.py
@@ -56,7 +56,7 @@ def analyze(in_distribution, out_of_distribution, channel_labels, is_image=False
 
     in_dist_shape = list(next(iter(in_distrbution_sample))[0].size())
     for i in (samples):
-        assert (list(next(iter(i))[0].size())[0] != in_dist_shape), ("Dimensions are not consistent, was looking for dimensions: "+str(in_dist_shape))
+        assert (list(next(iter(i))[0].size()) == in_dist_shape), ("Dimensions are not consistent, was looking for dimensions: "+str(in_dist_shape))
     print("Dimensions are consistent - Shape: "+str(in_dist_shape))
     
 
@@ -66,13 +66,22 @@ def analyze(in_distribution, out_of_distribution, channel_labels, is_image=False
 
     
     if is_image:
-        trans = transforms.ToPILImage()
-        # Conducting Approperiate Tests
-        
-        data = [trans((next(iter(in_distrbution_sample))[0]))]
+        data = [((next(iter(in_distrbution_sample))[0]))]
         for i in samples:
-            data.append(trans((next(iter(i))[0])))
+            data.append(((next(iter(i))[0])))
+            
+        print("Generating Graph Layout...")
+
+        tabs = []
+        for i, label in zip(data, data_labels):
+            i = i.cpu().detach().numpy()
+            tabs.append((label,pn.Column(dde.pixel_dist_img(i,channel_labels), 
+                                         dde.color_dist_img(i,channel_labels), 
+                                         dde.multi_dem_color_hist(i,channel_labels))))
+                
+        dashboard = pn.Column(dashboard_title,dashboard_desc, dde.param,  pn.Tabs(*tabs))
         print("Conducting the following comparisions for image data: Color distribution, Pixel distribution, and Variance of laplacian operators")
+    
     else:
         print("Conducting the following comparisions for signal data: Min/Max/Mean/StDev, Energy, and Power")
         assert(signal_frequency is not None), "Signal Frequency cannot be None"
